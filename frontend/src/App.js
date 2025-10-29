99b9ada0c243079d7f90103dc0e93a069160cad0
Thilinadranasinghe <thilinadr1@gmail.com>
2025-10-30 01:49:55 +0530

    Add App.js to the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Crypto Trading Dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
